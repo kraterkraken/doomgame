449ef998fd3df1b6f9636d953beb8a77ce28bdf6
kraterkraken <robxxiv@gmail.com>
2023-05-30 20:31:30 -0400

    completely removed old fin_wall function

--- RayCaster.py
+++ RayCaster.py
@@ -18,7 +18,7 @@ class RayCaster:
     def __init__(self, game):
         self.game = game
 
-    def find_wall2(self, x, y, angle):
+    def find_wall(self, x, y, angle):
 
         s = TILE_SIZE
         tanangle = math.tan(angle)
@@ -85,7 +85,7 @@ class RayCaster:
             angle += RAY_ANGLE
             ray_count += 1
 
-            endx, endy, c, r, depth = self.find_wall2(x, y, angle)
+            endx, endy, c, r, depth = self.find_wall(x, y, angle)
 
             # the closer away the wall, the brighter it will appear
             brightness = 255 - int(255 * depth/SCREEN_WIDTH)
@@ -164,97 +164,5 @@ class RayCaster:
  
     def is_in_bounds_cr(self, c, r):
         return (int(c), int(r)) in self.game.map.squares
-
-    def is_in_bounds(self, pos):
-        if (pos[0] > SCREEN_WIDTH
-        or pos[0] < 0
-        or pos[1] > SCREEN_HEIGHT
-        or pos[1] < 0):
-            return False
-        else:
-            return True
-
-    def find_wall(self, from_x, from_y, angle):
-        # This function finds the nearest wall hit by a ray
-        # cast from (from_x,from_y) and returns the following:
-        #
-        #   x :     horiz map coordinate where the ray hit the wall
-        #   y :     vert map coordinate where the way hit the wall
-        #   depth:  distance from (from_x,fomr_y) to the wall
-        #   col :   the map column where the ray hit the wall
-        #   row :   the map row where the ray hit the wall
-
-        # this was insanely hard to come up with
-
-        w = TILE_SIZE
-
-        cosangle = math.cos(angle)
-        sinangle = math.sin(angle)
-        tanangle = math.tan(angle)
-        signsin = sign(sinangle)
-        signcos = sign(cosangle)
-
-        go_col = cosangle != 0 # if we aren't going straight up / down, we can check "by col"
-        go_row = sinangle != 0 # if we aren't going straight left / right, we can check "by row"
-
-        # figure out the increments (wx, dy) for searching "by col"
-        h = w if cosangle == 0 else abs(w / cosangle)
-        dy = h * sinangle
-        wx = w * sign(cosangle) # the correct sign for moving width units in x direction
-
-        # figure out the increments (dx, wy) for searching "by row"
-        h = w if sinangle == 0 else abs(w / sinangle)
-        dx = h * cosangle
-        wy = w * sign(sinangle) # the correct sign for moving width units in y direction
-
-        # x_0 is the closest column edge to the "from"" in the direction of the ray
-        # y_0 is the closest row edge to the "from" in the directon of the ray
-        # Math: 
-        #   When sin/cos is positive we need to add 1 to the "from" row/col
-        #   When sin/cos is negative we need to add ZERO to the "from" row/col
-        #   To accomplish this I take the sign of each, add 0.5 and truncate to int.
-        from_c, from_r = xy_to_cr(from_x, from_y)
-        x_0 = w * (from_c + int(signcos + 0.5))
-        y_0 = w * (from_r + int(signsin + 0.5))
-
-        # by_col is the coordinates where the ray intersects a column edge
-        # by_row is the coordinates where the ray intersects a row edge
-        # (again, we are starting at the edges closest to the "from" in the ray's direction)
-        temp = from_y + (x_0 - from_x) * tanangle
-        by_col = [x_0, temp]
-        temp = 1 if cosangle == 0 else from_x + (y_0 - from_y) / tanangle # this prevents division by zero
-        by_row = [temp, y_0]
-
-        c, r = 0, 0
-        while go_col and self.is_in_bounds(by_col):
-            # did we hit a wall "by column"?
-            is_in_wall, c, r = self.game.map.is_in_wall(by_col[0] + signcos, by_col[1])
-            if is_in_wall:
-                break;
-            else:
-                by_col[0] += wx
-                by_col[1] += dy
-                
-        while go_row and self.is_in_bounds(by_row):
-            # did we hit a wall "by row"?
-            is_in_wall, c, r = self.game.map.is_in_wall(by_row[0], by_row[1] + signsin)
-            if is_in_wall:
-                break;
-            else:
-                by_row[0] += dx
-                by_row[1] += wy
-
-        dist_by_col = depth(from_x, from_y, by_col[0], by_col[1])
-        dist_by_row = depth(from_x, from_y, by_row[0], by_row[1])
-
-        wallpos = by_row
-        dist = dist_by_row
-        if dist_by_col < dist_by_row:
-            wallpos = by_col
-            dist = dist_by_col
-
-        c, r = xy_to_cr(wallpos[0], wallpos[1])
-
-        return wallpos[0], wallpos[1], c, r, dist
 
 
