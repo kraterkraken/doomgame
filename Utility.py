3863c0a344df6612f253816f49c4f148a8486f34
kraterkraken <robxxiv@gmail.com>
2023-05-30 20:25:53 -0400

    Added wrap_angle() and get_cr_dir() to give us the direction of an angle in terms of unit vectors

--- Utility.py
+++ Utility.py
@@ -1,6 +1,10 @@
 import math
 from Settings import *
 
+
+def wrap_angle(angle):
+    return angle % (2.0*math.pi)
+
 def sign(x):
     if x == 0: return 0
     if x > 0: return 1
@@ -14,4 +18,43 @@ def xy_to_cr(x, y):
     # convert a map x-y coordinate into the 
     # row and column of the tile that coordinate is inside of
     return x//TILE_SIZE, y//TILE_SIZE
+
+def get_cr_dir(angle):
+    # what direction are we going?
+
+    angle = wrap_angle(angle)
+
+    c_incr = 0
+    r_incr = 0
+
+    if angle == 0 or angle == TWO_PI:
+        # east
+        c_incr = 1
+    elif angle == HALF_PI:
+        # south
+        r_incr = 1
+    elif angle == math.pi:
+        # west
+        c_incr = -1
+    elif angle == HALF_3PI:
+        # north
+        r_incr = -1
+    elif 0 < angle and angle < HALF_PI:
+        # south east
+        c_incr = 1
+        r_incr = 1
+    elif HALF_PI < angle and angle < math.pi:
+        # south west
+        c_incr = -1
+        r_incr = 1
+    elif math.pi < angle and angle < HALF_3PI:
+        # north west
+        c_incr = -1
+        r_incr = -1
+    elif HALF_3PI < angle and angle < TWO_PI:
+        # north east
+        c_incr = 1
+        r_incr = -1
+    
+    return c_incr, r_incr
 
