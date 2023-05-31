3389656a3a811372a6862d57da57bea37ed158df
kraterkraken <robxxiv@gmail.com>
2023-05-30 20:28:37 -0400

    Made new function to find wall.  New one is c/r-based instead of x/y-based, and works a ton better.

--- RayCaster.py
+++ RayCaster.py
@@ -17,7 +17,62 @@ class RayCaster:
 
     def __init__(self, game):
         self.game = game
-        self.wall_chunks = []
+
+    def find_wall2(self, x, y, angle):
+
+        s = TILE_SIZE
+        tanangle = math.tan(angle)
+        cotangle = 1 / tanangle
+        x_div_s = x / s # real col coord, i.e. 4.35 is 35% of the way across col #4
+        y_div_s = y / s # real row coord, i.e. 3.22 is 22% of the way across row #3
+        pix = 1 / s  # the pct of a col or row that equals 1 pixel
+
+        # what tile are we in?
+        c, r = xy_to_cr(x, y)
+
+        # what direction are we going?
+        c_incr, r_incr = get_cr_dir(angle)
+
+        # get starting coords
+        c1 = c + c_incr if c_incr > 0 else c - pix
+        r1 = y_div_s + (c1 - x_div_s) * tanangle
+
+        r2 = r + r_incr if r_incr > 0 else r - pix
+        c2 = x_div_s + (r2 - y_div_s) / tanangle # TODO: DIV BY ZERO CHECK!!
+
+        # get deltas
+        dc = abs(cotangle) * c_incr
+        dr = abs(tanangle) * r_incr
+
+        # find wall by column
+        while self.is_in_bounds_cr(c1, r1):
+            # did we hit a wall
+            if self.game.map.squares[(int(c1), int(r1))].is_wall:
+                break;
+            else:
+                c1 += c_incr
+                r1 += dr
+
+        # find wall by row
+        while self.is_in_bounds_cr(c2, r2):
+            # did we hit a wall
+            if self.game.map.squares[(int(c2), int(r2))].is_wall:
+                break;
+            else:
+                c2 += dc
+                r2 += r_incr
+
+        # pick the wall coords that are the smallest distance away
+        dist1 = depth(x_div_s, y_div_s, c1, r1)
+        dist2 = depth(x_div_s, y_div_s, c2, r2)
+        answer = (c1, r1)
+        dist = dist1
+        if dist2 < dist1:
+            answer = (c2, r2)
+            dist = dist2
+
+        # return the x,y and c,r coords where we hit wall, and the depth to wall
+        return (answer[0]*s, answer[1]*s, int(answer[0]), int(answer[1]), dist*s)  
 
 
     def ray_cast(self, x, y, heading):
@@ -30,7 +85,7 @@ class RayCaster:
             angle += RAY_ANGLE
             ray_count += 1
 
-            endx, endy, depth, c, r = self.find_wall(x, y, angle)
+            endx, endy, c, r, depth = self.find_wall2(x, y, angle)
 
             # the closer away the wall, the brighter it will appear
             brightness = 255 - int(255 * depth/SCREEN_WIDTH)
@@ -50,7 +105,7 @@ class RayCaster:
             if TOPDOWN:
                 pygame.draw.line(self.game.screen, "yellow", (x, y), (endx, endy), 2)
                 continue
-            if PLAINWALL: # or not (c == 6 and r == 1):
+            if PLAINWALL: # or not (c == 6 and r == 0):
                 # Draw a very narrow vertical rectangle to represent the portion
                 # of the wall ("wall chunk") that the current ray hit.  The 
                 # farther away it is the shorter the wall chunk will be, which  
@@ -74,36 +129,42 @@ class RayCaster:
             # player, so for the 4 cases the rays scan (1) down, (2) right,
             # (3) up, and (4) left.  
 
-
             # Starting in the upper left of the tile and going counter-clockwise,
             # I'm going to label the corners A, B, C, D:
 
             w = TILE_SIZE
             Ax, Ay = (w*c, w*r)
-            Bx, By = (Ax, Ay+w)
-            Cx, Cy = (Ax+w, By)
+            Bx, By = (Ax, Ay+w-1)
+            Cx, Cy = (Ax+w-1, By)
             Dx, Dy = (Cx, Ay)
 
             offset = 0
-            if (endx == Ax):
-                # CASE 1
+            if (int(endx) == Ax):
+                # CASE 1 - left side of 2D tile
                 offset = endy - Ay
-            elif (endy == By):
-                # CASE 2
+            elif (int(endy) == By):
+                # CASE 2 - bottom side of 2D tile
                 offset = endx - Bx
-            elif (endx == Cx):
-                # CASE 3
+            elif (int(endx) == Cx):
+                # CASE 3 - right side of 2D tile
                 offset = Cy - endy
-            elif (endy == Dy):
-                # CASE 4
+            elif (int(endy) == Dy):
+                # CASE 4 - top side of 2D tile
                 offset = Dx - endx
+            else:
+                print("HUUUGE ERROR: Could not determine whihc side of tile a ray hit!!")
+                pass
 
+            texture_id = "B" #self.game.map.squares[c,r].texture_id
             self.game.graphics.draw_wall_chunk(
-                "brickwall", 
+                texture_id, 
                 (screen_x, screen_y, WALL_CHUNK_WIDTH, wall_height), 
                 offset,
                 brightness)
  
+    def is_in_bounds_cr(self, c, r):
+        return (int(c), int(r)) in self.game.map.squares
+
     def is_in_bounds(self, pos):
         if (pos[0] > SCREEN_WIDTH
         or pos[0] < 0
@@ -164,9 +225,11 @@ class RayCaster:
         temp = 1 if cosangle == 0 else from_x + (y_0 - from_y) / tanangle # this prevents division by zero
         by_row = [temp, y_0]
 
+        c, r = 0, 0
         while go_col and self.is_in_bounds(by_col):
             # did we hit a wall "by column"?
-            if self.game.map.is_in_wall(by_col[0] + signcos, by_col[1]):
+            is_in_wall, c, r = self.game.map.is_in_wall(by_col[0] + signcos, by_col[1])
+            if is_in_wall:
                 break;
             else:
                 by_col[0] += wx
@@ -174,7 +237,8 @@ class RayCaster:
                 
         while go_row and self.is_in_bounds(by_row):
             # did we hit a wall "by row"?
-            if self.game.map.is_in_wall(by_row[0], by_row[1] + signsin):
+            is_in_wall, c, r = self.game.map.is_in_wall(by_row[0], by_row[1] + signsin)
+            if is_in_wall:
                 break;
             else:
                 by_row[0] += dx
@@ -190,6 +254,7 @@ class RayCaster:
             dist = dist_by_col
 
         c, r = xy_to_cr(wallpos[0], wallpos[1])
-        return wallpos[0], wallpos[1], dist, c, r
+
+        return wallpos[0], wallpos[1], c, r, dist
 
 
