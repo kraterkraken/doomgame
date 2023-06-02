5edb39b0cf55771e8f0acbaaa99cd41bc52bce9c
kraterkraken <robxxiv@gmail.com>
2023-06-01 22:16:18 -0400

    removed unused functions

--- Utility.py
+++ Utility.py
@@ -5,15 +5,6 @@ from Settings import *
 def wrap_angle(angle):
     return angle % (2.0*math.pi)
 
-def sign(x):
-    if x == 0: return 0
-    if x > 0: return 1
-    if x < 0: return -1
-
-def depth(x0, y0, x1, y1):
-    # pythagorean theorem
-    return math.sqrt((x1-x0)**2 + (y1-y0)**2)
-
 def xy_to_cr(x, y):
     # convert a map x-y coordinate into the 
     # row and column of the tile that coordinate is inside of
